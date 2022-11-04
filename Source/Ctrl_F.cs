68ceee4a57eab209dfa3a55a6394559463349ff9
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-11-03 19:56:50 -0700

    Empty Project.

---
+++ Source/Ctrl_F.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Linq;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace Ctrl_F
+{
+	public class Mod : Verse.Mod
+	{
+		public Mod(ModContentPack content) : base(content)
+		{
+		}
+	}
+}
