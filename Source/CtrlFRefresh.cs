bee0016f2793f480275a3dd11a90a2686a8c0dc9
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-11-16 13:48:11 -0800

    Implement OpenUI for refresher

--- Source/CtrlFRefresh.cs
+++ Source/CtrlFRefresh.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 using TD_Find_Lib;
 
 namespace Ctrl_F
@@ -11,6 +12,20 @@ namespace Ctrl_F
 	{
 		public CtrlFRefresh(FindDescription desc): base(desc, "Ctrl-F", 60, false)
 		{
+		}
+
+		public override void OpenUI(FindDescription desc)
+		{
+			if (Current.Game.GetComponent<CtrlFGameComponent>().window is CtrlFWindowSearch window)
+			{
+				if (Find.WindowStack.IsOpen(window))
+					Find.WindowStack.Notify_ClickedInsideWindow(window);
+				else
+					Find.WindowStack.Add(window);
+			}
+			//Ctrl-F UI should be open if there is a refreshing list.
+			//else
+			//	CtrlFWindowSearch.OpenWith(desc);
 		}
 	}
 }
